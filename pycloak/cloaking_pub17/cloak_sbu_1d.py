0a131fc3ff36f6a62065aa97974e969164adcf11
Nils Feege <nils.feege@stonybrook.edu>
2017-02-16 14:09:25 -0500

    Add plots

--- pycloak/cloaking_pub17/cloak_sbu_1d.py
+++ pycloak/cloaking_pub17/cloak_sbu_1d.py
@@ -28,7 +28,7 @@ def comparescans( figname, figtitle, datafiles, datanames, legend=True ):
     for i, (dfile, dname) in enumerate( zip( datafiles, datanames ) ):
         print( dfile, dname )
         data = pd.read_csv(dfile, comment='#')
-        ax.plot( data['pos'], abs(data['B1']), marker='o', label=dname) #color='b'
+        ax.plot( data['pos'], abs(data['B1']), marker='.', label=dname) #color='b'
 
         # fill areas between curves and reference value
         #Bnominal = abs(data['B2']).mean()
@@ -41,6 +41,7 @@ def comparescans( figname, figtitle, datafiles, datanames, legend=True ):
     #ax.set_xlim(-12000, 12000)
     ax.set_ylabel('B (mT)')
     #ax.set_ylim(0,500)
+    ax.set_yscale("log", nonposy='clip')
 
     # add legend
     if ( legend ):
@@ -74,5 +75,44 @@ if __name__ == '__main__':
                   ["fm = 0.548",
                    "fm = 0.574",
                    "reference"])
+
+
+    # 45-layer SC and FM=0.618 ferromagnet, 40 mT
+    # BvX, 1mm from edge of cloak (y=101mm): DataFile_170214_205838.txt
+    # BvX, 1cm from edge of cloak (y=110mm): DataFile_170214_210939.txt
+
+    # 45-layer SC and FM=0.554 ferromagnet, 40 mT
+    #...
+
+    # 4-layer SC and FM=0.618 ferromagnet, 40 mT
+    # BvX, 1 mm away from edge @ z-center: DataFile_170213_194029.txt
+    # BvX, 1 cm away from edge @ z-center: DataFile_170213_201438.txt
+
+    # BvX reference 1mm away from edge @ z-center: DataFile_170213_204446.txt
+    # BvX reference 1cm away from edge @ z-center: DataFile_170213_205815.txt
+
+    # Compare SC4+FM618, SC45+FM618 at 40 mT
+    comparescans( "plots/cloak_sbu_compare_sc_1.png" ,
+                  "Cloaking at 40 mT (field measured 1 cm in front of prototype)",
+                  ["data-calib/DATA_Gaussmeter/DataFile_170213_201438.csv",
+                   "data-calib/DATA_Gaussmeter/DataFile_170214_210939.csv",
+#                   "data-calib/DATA_Gaussmeter/DataFile_170203_204341.csv",
+                   "data-calib/DATA_Gaussmeter/DataFile_170213_205815.csv"],
+                  [" 4-layer SC + fm618",
+                   "45-layer SC + fm618",
+#                   "fm = SC45 + fm574",
+                   "reference"])
+
+
+
+    # Compare SC4 + SC45 BvZ
+    comparescans( "plots/cloak_sbu_compare_sc_2.png" ,
+                  "Shielding at 40 mT (field measured in center of SC)",
+                  ["data-calib/DATA_Gaussmeter/DataFile_170215_181018.csv",
+                   "data-calib/DATA_Gaussmeter/DataFile_170215_184713.csv",
+                   "data-calib/DATA_Gaussmeter/DataFile_170214_201515.csv"],
+                  [" 4-layer SC",
+                   "45-layer SC",
+                   "no SC shield"])
 
                   
--- pycloak/cloaking_pub17/cloak_sbu_subplots_1d.py
+++ pycloak/cloaking_pub17/cloak_sbu_subplots_1d.py
@@ -7,52 +7,107 @@ import matplotlib.pyplot as plt
 
 from matplotlib import cm
 
-
-
-
 # make plot 1
-fig, axs = plt.subplots(3,1,figsize=(6,6))
+fig, axs = plt.subplots(3,1,figsize=(6,6),sharex=True)
 
-# BvX, 1 mm away from edge @ z-center: DataFile_170213_194029.txt
-df_bvx_cloak_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_194029.csv')
-
-# BvX, 1 cm away from edge @ z-center: DataFile_170213_201438.txt
-df_bvx_cloak_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_201438.csv')
-
-# BvX along axis of cloak @ z-center: DataFile_170213_202411.txt
-df_bvx_cloak_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_202411.csv')
-
+# 4 layer SC + fm618 BvX along axis of cloak @ z-center: DataFile_170213_202411.txt
+# 4 layer SC + fm618 BvX, 1 mm away from edge @ z-center: DataFile_170213_194029.txt
+# 4 layer SC + fm618 BvX, 1 cm away from edge @ z-center: DataFile_170213_201438.txt
 # BvX reference along axis of cloak @ z-center: DataFile_170213_203450.txt
-df_bvx_ref_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_203450.csv')
-
 # BvX reference 1mm away from edge @ z-center: DataFile_170213_204446.txt
-df_bvx_ref_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_204446.csv')
-
 # BvX reference 1cm away from edge @ z-center: DataFile_170213_205815.txt
+# 4 layer SC only: BvsX along cloak axis at z-center (,y = 78mm, z = 71mm): DataFile_170215_162938.txt
+# 4 layer SC only: BvsX 1mm from surface at z-center (, y = 103mm, z = 71mm): DataFile_170215_163835.txt
+# 4 layer SC only: BvsX 33.1 mm from cloak center in z-center (y = 111mm, z = 71mm): DataFile_170215_165018.txt
+
+
+df_bvx_cloak_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_202411.csv')
+df_bvx_cloak_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_194029.csv')
+df_bvx_cloak_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_201438.csv')
+
+df_bvx_sc4l_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_162938.csv')
+df_bvx_sc4l_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_163835.csv')
+df_bvx_sc4l_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_165018.csv')
+
+df_bvx_ref_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_203450.csv')
+df_bvx_ref_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_204446.csv')
 df_bvx_ref_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170213_205815.csv')
 
 axs[0].set_title("B vs X scans, 4 layer SC, fM = 0.618")
-axs[0].plot( df_bvx_cloak_2['pos'], abs(df_bvx_cloak_2['B1']), marker='o', label='1 cm away') 
-axs[0].plot( df_bvx_ref_2['pos'], abs(df_bvx_ref_2['B1']), marker='o', label='1 cm away', alpha=0.4) 
+axs[0].plot( df_bvx_cloak_1['pos'], abs(df_bvx_cloak_1['B1']), marker='.', label='across center') 
+axs[0].plot( df_bvx_sc4l_1['pos'], abs(df_bvx_sc4l_1['B1']), marker='.', label='across center') 
+axs[0].plot( df_bvx_ref_1['pos'], abs(df_bvx_ref_1['B1']), marker='.', label='across center', alpha=0.4) 
 axs[0].set_ylabel("$B$ (mT)")
-axs[0].set_ylim((38,40.5))
-axs[0].text(175,38.2,'1 cm away',fontsize=12)
+#axs[0].set_ylim((38,40.5))
+axs[0].text(175,38.2,'across',fontsize=12)
 
-axs[1].plot( df_bvx_cloak_1['pos'], abs(df_bvx_cloak_1['B1']), marker='o', label='1 mm away') 
-axs[1].plot( df_bvx_ref_1['pos'], abs(df_bvx_ref_1['B1']), marker='o', label='1 mm away', alpha=0.4) 
+axs[1].plot( df_bvx_cloak_2['pos'], abs(df_bvx_cloak_2['B1']), marker='.', label='1 mm away') 
+axs[1].plot( df_bvx_sc4l_2['pos'], abs(df_bvx_sc4l_2['B1']), marker='.', label='across center') 
+axs[1].plot( df_bvx_ref_2['pos'], abs(df_bvx_ref_2['B1']), marker='.', label='1 mm away', alpha=0.4) 
 axs[1].set_ylabel("$B$ (mT)")
-axs[1].set_ylim((38,40.5))
+#axs[1].set_ylim((38,40.5))
 axs[1].text(175,38.2,'1 mm away',fontsize=12)
 
-axs[2].plot( df_bvx_cloak_3['pos'], abs(df_bvx_cloak_3['B1']), marker='o', label='across') 
-axs[2].plot( df_bvx_ref_3['pos'], abs(df_bvx_ref_3['B1']), marker='o', label='across', alpha=0.4) 
+axs[2].plot( df_bvx_cloak_3['pos'], abs(df_bvx_cloak_3['B1']), marker='.', label='1 cm away') 
+axs[2].plot( df_bvx_sc4l_3['pos'], abs(df_bvx_sc4l_3['B1']), marker='.', label='across center') 
+axs[2].plot( df_bvx_ref_3['pos'], abs(df_bvx_ref_3['B1']), marker='.', label='1 cm away', alpha=0.4) 
 axs[2].set_ylabel("$B$ (mT)")
 axs[2].set_xlabel("position (mm)")
-axs[2].set_ylim((38,40.5))
-axs[2].text(175,38.2,'across',fontsize=12)
-
-#plt1=axs[1].plot( data.loc[data['tdep']==False,'Bout'], data.loc[data['tdep']==False,'Bins'], marker='o')
-#axs[1].errorbar( data.loc[data['tdep'],'Bout'], data.loc[data['tdep'],'Bins'], yerr=data.loc[data['tdep'],'Bins_sdev'].values, marker='o', color=plt1[0].get_color(), mfc='white')
+#axs[2].set_ylim((38,40.5))
+axs[2].text(175,38.2,'1 cm away',fontsize=12)
 
 plt.savefig("plots/cloak_sbu_bvx_1.png")
+plt.show()
+###############################################
+
+# make plot 2
+fig, axs = plt.subplots(3,1,figsize=(6,6),sharex=True)
+
+# BvZ, through the cloak axis @ x,y-center: DataFile_170214_185331.txt
+# BvZ, 1mm away from edge @ y-center: DataFile_170214_190443.txt (x=118mm)
+# BvZ, 1mm away from edge @ x-center: DataFile_170214_191525.txt (y=105mm)
+# BvZ reference, through the cloak axis @ x,y-center: DataFile_170214_201515.txt
+# BvZ reference, 1mm away from edge @ y-center: DataFile_170214_202512.txt (x=118mm)
+# BvZ reference, 1mm away from edge @ x-center: DataFile_170214_203513.txt (y=105mm)
+# BvsZ SC only through center of cloak (y = 75mm, x = 142mm): DataFile_170215_181018.txt
+# BvsZ SC only 1 mm from cloak edge in z top (y = 103mm, x = 142mm): DataFile_170215_173242.txt
+# BvsZ SC only 1mm from cloak edge in x (y=75m, x=113mm): DataFile_170215_182257.txt
+
+df_bvz_cloak_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_185331.csv')
+df_bvz_cloak_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_190443.csv')
+df_bvz_cloak_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_191525.csv')
+
+df_bvz_sc4l_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_181018.csv')
+df_bvz_sc4l_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_173242.csv')
+df_bvz_sc4l_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170215_182257.csv')
+
+df_bvz_ref_1 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_201515.csv')
+df_bvz_ref_2 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_202512.csv')
+df_bvz_ref_3 = pd.read_csv('data-calib/DATA_Gaussmeter/DataFile_170214_203513.csv')
+
+axs[0].set_title("B vs Z scans, 4 layer SC, fM = 0.618")
+axs[0].plot( df_bvz_cloak_1['pos'], abs(df_bvz_cloak_1['B1']), marker='.', label='1 cm away') 
+axs[0].plot( df_bvz_sc4l_1['pos'], abs(df_bvz_sc4l_1['B1']), marker='.', label='1 cm away', alpha=0.4) 
+axs[0].plot( df_bvz_ref_1['pos'], abs(df_bvz_ref_1['B1']), marker='.', label='1 cm away', alpha=0.4) 
+axs[0].set_ylabel("$B$ (mT)")
+#axs[0].set_yscale("log", nonposy='clip')
+#axs[0].set_ylim((38,40.5))
+axs[0].text(-50,20,'x,y center',fontsize=12)
+
+axs[1].plot( df_bvz_cloak_2['pos'], abs(df_bvz_cloak_2['B1']), marker='.', label='1 mm away') 
+axs[1].plot( df_bvz_sc4l_2['pos'], abs(df_bvz_sc4l_2['B1']), marker='.', label='1 cm away', alpha=0.4) 
+axs[1].plot( df_bvz_ref_2['pos'], abs(df_bvz_ref_2['B1']), marker='.', label='1 mm away', alpha=0.4) 
+axs[1].set_ylabel("$B$ (mT)")
+#axs[1].set_ylim((36,44))
+axs[1].text(-50,42,'y center',fontsize=12)
+
+axs[2].plot( df_bvz_cloak_3['pos'], abs(df_bvz_cloak_3['B1']), marker='.', label='across') 
+axs[2].plot( df_bvz_sc4l_3['pos'], abs(df_bvz_sc4l_3['B1']), marker='.', label='1 cm away', alpha=0.4) 
+axs[2].plot( df_bvz_ref_3['pos'], abs(df_bvz_ref_3['B1']), marker='.', label='across', alpha=0.4) 
+axs[2].set_ylabel("$B$ (mT)")
+axs[2].set_xlabel("position (mm)")
+#axs[2].set_ylim((36,44))
+axs[2].text(-50,42,'x center',fontsize=12)
+
+plt.savefig("plots/cloak_sbu_bvz_1.png")
 plt.show()
